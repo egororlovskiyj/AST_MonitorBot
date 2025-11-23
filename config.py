b0728e11734d6e2a164a4d03c062f4119f9373f2
egororlovskiyj <egororlovskiyj@gmail.com>
2025-11-23 22:01:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = int(os.getenv("CHAT_ID"))
+CHAT_ID = int(os.getenv("TG_CHAT"))
 DATABASE_URL = os.getenv("DATABASE_URL")  # PostgreSQL URL от Railway
 TIMEZONE = "Europe/Kiev"
+
