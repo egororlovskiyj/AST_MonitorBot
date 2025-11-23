b119f13dc6ad63b559ad9f2d3b03ae3610f55210
egororlovskiyj <egororlovskiyj@gmail.com>
2025-11-23 22:32:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = int(os.getenv("TG_CHAT"))
+CHAT_ID = int(str(os.getenv("TG_CHAT")).strip())
 DATABASE_URL = os.getenv("DATABASE_URL")
 TIMEZONE = "Europe/Kiev"
+
 
 
 
