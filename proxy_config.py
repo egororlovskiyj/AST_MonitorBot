f69a3a87c0954ec244c4a86479e4bd4c6b9be93b
egororlovskiyj <egororlovskiyj@gmail.com>
2025-11-24 10:43:46 +0200

    proxy_config.py

--- proxy_config.py
+++ proxy_config.py
@@ -1,8 +1,1 @@
-# proxy_config.py
 
-PROXY_HOST = "5y7uqt2ob5.cn.fxdx.in"
-PROXY_PORT = "15469"
-PROXY_USER = "originalharmony271109"
-PROXY_PASS = "eWvTseqkohn5"
-
-PROXY = f"socks5://originalharmony271109:eWvTseqkohn5@5y7uqt2ob5.cn.fxdx.in:15469"
