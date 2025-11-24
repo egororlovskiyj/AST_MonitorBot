03f2713d3fd003143ef03106abdb722f7e41e53c
egororlovskiyj <egororlovskiyj@gmail.com>
2025-11-24 09:38:51 +0200

    proxy_config.py

--- proxy_config.py
+++ proxy_config.py
@@ -3,6 +3,6 @@
 PROXY_HOST = "5y7uqt2ob5.cn.fxdx.in"
 PROXY_PORT = "15469"
 PROXY_USER = "originalharmony271109"
-PROXY_PASS = "eWvTse"
+PROXY_PASS = "eWvTseqkohn5"
 
 PROXY = f"socks5://5y7uqt2ob5.cn.fxdx.in:15469:originalharmony271109:eWvTseqkohn5"
