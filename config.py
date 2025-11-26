13e0e3574870aa83bc86bc0a4b6568fc28d96bc2
egororlovskiyj <egororlovskiyj@gmail.com>
2025-11-26 18:15:15 +0200

    config.py

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 DATABASE_URL = os.getenv("DATABASE_URL")
 TIMEZONE = os.getenv("TIMEZONE", "Europe/Kyiv")
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
+RAPIDAPI_HOST = os.getenv("RAPIDAPI_HOST", "instagram-scraper-stable-api.p.rapidapi.com")
 
 
 
