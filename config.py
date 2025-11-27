327d2a92a1eb3cd3a372fc8a229040d0d09ea62d
egororlovskiyj <egororlovskiyj@gmail.com>
2025-11-27 09:40:36 +0200

    config.py

--- config.py
+++ config.py
@@ -1,22 +1,13 @@
 import os
 
-# Telegram
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = int(os.getenv("CHAT_ID"))
-
-# База данных (Postgres на Railway)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Часовой пояс для отчёта (можешь поменять при желании)
 TIMEZONE = os.getenv("TIMEZONE", "Europe/Helsinki")
 
-# RapidAPI – Instagram Scraper Stable API
 RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-RAPIDAPI_HOST = os.getenv(
-    "RAPIDAPI_HOST",
-    "instagram-scraper-stable-api.p.rapidapi.com"
-)
-
+RAPIDAPI_HOST = os.getenv("RAPIDAPI_HOST", "instagram-scraper-stable-api.p.rapidapi.com")
 
 
 
